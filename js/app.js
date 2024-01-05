1271d222e58e20c982c26d00231b9dd2765c60c2
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-01-05 06:35:39 -0500

    creacion de la funcionalidad de listar productos

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
-let productosCarrito = [{
-    id: "1",
-    titulo: undefined,
-    imagen: undefined,
-    precio: undefined,
-    cantidad: 1,
-}]
+let productosCarrito = []
 const listaProductos = document.getElementById("lista-productos")
 const carrito = document.getElementById("carrito")
 const vaciarCarrito = document.getElementById("vaciar-carrito")
+const tbodyCarrito = document.querySelector("#lista-carrito tbody")
 
 listaProductos.addEventListener('click', function (event) {
     if (event.target.classList.contains("agregar-carrito")) {
@@ -36,9 +31,31 @@ function agregarProductosAlCarrito(id, card) {
 
     const busquedaProducto = productosCarrito.find(element => element.id === producto.id)
 
-    if (busquedaProducto) {
-        busquedaProducto.cantidad++
-    } else {
+    if (busquedaProducto === undefined) {
         productosCarrito.push(producto)
+    } else {
+        busquedaProducto.cantidad++
     }
+
+    listarProductosDelCarrito()
+}
+
+
+function listarProductosDelCarrito() {
+    tbodyCarrito.innerHTML = ""
+    productosCarrito.forEach(function (producto) {
+        tbodyCarrito.innerHTML += `
+            <tr>
+                <td>
+                    <img height="100" src="${producto.imagen}"/>
+                </td>
+                <td>${producto.titulo}</td>
+                <td>${producto.cantidad}</td>
+                <td>${producto.precio}</td>
+                <td>
+                    <button class="eliminar-producto btn btn-danger" data-id="${producto.id}">Eliminar</button>
+                </td>
+            </tr>
+        `;
+    })
 }
