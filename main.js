4e872a31d22d1fdd405e75e8cd46b65e61171b32
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-01-06 16:49:03 -0500

    avances en la implementacion de bootstrap

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+import './src/scss/custom.scss'
+import './src/scss/normalize.scss'
+import './src/scss/skeleton.scss'
+
+import * as bootstrap from 'bootstrap'
+
 import {main} from "./src/js/app"
 
 main
